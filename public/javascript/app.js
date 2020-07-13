b2a64bdf5727c4163c52a82e767450e076ece7e8
unknown <jonathang@hotmail.com.ar>
2020-07-13 00:18:16 -0300

    Arreglando detalle producto

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -6,17 +6,23 @@ var front = document.querySelector('.front');
 
 
 
+
 btnEnviar.addEventListener("click",function(event){
-    event.preventDefault()
+  let errores=[];
     if(formulario[0].value == "" || formulario[0].value == null){
-       front.innerHTML = '<div clas="modal">❗El campo Nombre esta vacio❗</div>'
-       if(formulario[1].value == "" || formulario[1].value == null){
-        front.innerHTML = '<p>❗El campo Apellido esta vacio❗</p>'
-     }
+      errores.push('❗<p>El campo Nombre esta vacio❗</p>');
     }
-    
-     
+           if(formulario[1].value == "" || formulario[1].value == null){
+            errores.push('<p>❗El campo Apellido esta vacio❗</p>')
+         }
+         if (errores.length > 0) {
+           event.preventDefault();
+           errores.forEach((erroress) => {
+             front.innerHTML += erroress;
+           });
+         }    
 })
+
 
 console.log(formulario)
 console.log(front)
